c10ca5f9fc1069d39db2bc7e84c1c21eb7147a14
Arwin Kumaramangalam <arwinkumaramangalam@Anish-Mac.local>
2020-12-22 03:25:20 +0530

    fisrt section

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log('DOM Loaded')
+
+let movementDisplay = movement
+let game = document.getElementById('game')
+
+game.getAttribute['width', '800px']
+game.height = '400'
+
+let ctx = game.getContext('2d')
+
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'red'
+ctx.lineWidth = 5
+
+ctx.fillRect(10, 10, 100, 100)
+ctx.strokeRect(10, 10, 100, 100) 
