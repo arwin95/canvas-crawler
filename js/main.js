af8619d519363428594af0f1c4fd92daac3affe2
Arwin Kumaramangalam <arwinkumaramangalam@Anish-Mac.local>
2020-12-22 03:26:23 +0530

    mini-update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 console.log('DOM Loaded')
-
+//
 let movementDisplay = movement
+//same as let 
+//movementDisplay = document.getElementById('movement')
 let game = document.getElementById('game')
 
+//
 game.getAttribute['width', '800px']
 game.height = '400'
 
+//get some context
 let ctx = game.getContext('2d')
 
+//changing Attribute
 ctx.fillStyle = 'white'
 ctx.strokeStyle = 'red'
 ctx.lineWidth = 5
 
+//
 ctx.fillRect(10, 10, 100, 100)
 ctx.strokeRect(10, 10, 100, 100) 
